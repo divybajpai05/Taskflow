d808a213974c1af55c1e6c3b63799bb7a43aa294
Prashant Thakur <prashantthakur.connect@gmail.com>
2026-05-03 12:46:31 +0530

    change static value to dynamic

--- client/src/pages/VerifyEmail.tsx
+++ client/src/pages/VerifyEmail.tsx
@@ -42,7 +42,7 @@ export default function VerifyEmail() {
 
       try {
         const response = await fetch(
-          "http://localhost:5000/api/auth/verify-email",
+          `${import.meta.env.VITE_API_URL}/auth/verify-email`,
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
